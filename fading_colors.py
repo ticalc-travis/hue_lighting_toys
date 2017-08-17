ab202c078e31830cee6d05e341d2e998895f0beb
Travis Evans <travisgevans@gmail.com>
2017-08-16 22:41:38 -0500

    Update fading_colors.py to use base module

--- fading_colors.py
+++ fading_colors.py
@@ -2,109 +2,85 @@
 
 import argparse
 import random
-import signal
 import sys
 import time
 
 import phue                     # https://github.com/studioimaginaire/phue
 
-def get_bridge(args):
-    return phue.Bridge(args.bridge_address)
+from base import (BaseProgram, ProgramArgumentError, run_with_quit_handler)
 
-def get_lights(bridge, args):
-    if args.lights:
-        lights = []
-        for ll in args.lights:
-            for li in ll:
-                try:
-                    lights.append(bridge[li])
-                except KeyError:
-                    print("Warning: No such light: %s" % repr(li), file=sys.stderr)
-    else:
-        lights = bridge.get_light_objects()
-    return lights
 
-def turn_on_lights(bridge, lights):
-    for L in lights:
-        bridge.set_light(L.light_id, 'on', True)
+class FadingColorsProgram(BaseProgram):
 
-def run_show(bridge, args, lights):
-    while True:
-        h = random.randint(*args.hue_range)
-        s = random.randint(*args.sat_range)
-        b = random.randint(*args.bri_range)
-        for L in lights:
-            bridge.set_light(L.light_id, {'hue': h, 'sat': s, 'bri': b},
-                             transitiontime=args.cycle_time)
-            if not args.group_hue:
-                h = random.randint(*args.hue_range)
-            if not args.group_sat:
-                s = random.randint(*args.sat_range)
-            if not args.group_bri:
-                b = random.randint(*args.bri_range)
-        time.sleep(args.cycle_time / 10)
+    usage_description = "Produce a Philips Hue lighting random color fade effect."
+
+    def _get_range_parser(self):
+        """Return a parser with options for hue/saturation/brightness ranges"""
+        parser = argparse.ArgumentParser(add_help=False)
+        parser.add_argument(
+            '-hr', '--hue-range',
+            help='restrict the generated hue range (0 to 65535) from L to H',
+            dest='hue_range', nargs=2, type=int, metavar=('L', 'H'),
+            default=[0, 65535])
+        parser.add_argument(
+            '-sr', '--saturation-range',
+            help='restrict the generated saturation range (0 to 255) from L to H',
+            dest='sat_range', nargs=2, type=int, metavar=('L', 'H'),
+            default=[0, 255])
+        parser.add_argument(
+            '-br', '--brightness-range',
+            help='restrict the generated saturation range (1 to 254) from L to H',
+            dest='bri_range', nargs=2, type=int, metavar=('L', 'H'),
+            default=[1, 254])
+        return parser
+
+    def _get_arg_parser(self):
+        parent = BaseProgram._get_arg_parser(self)
+        parent2 = self._get_range_parser()
+        parser = argparse.ArgumentParser(parents=[parent, parent2],
+                                         add_help=False)
+        parser.add_argument(
+            '-t', '--cycle-time',
+            help='''use a speed of %(metavar)s tenths of a second per color cycle
+(default: %(default)s)')''',
+            dest='cycle_time', type=int, metavar='DECISECONDS', default=100)
+        parser.add_argument(
+            '-gh', '--group-hue',
+            help='match the same hue among all lights',
+            dest='group_hue', action='store_true')
+        parser.add_argument(
+            '-gs', '--group-saturation',
+            help='match the same saturation among all lights',
+            dest='group_sat', action='store_true')
+        parser.add_argument(
+            '-gb', '--group-brightness',
+            help='match the same brightness among all lights',
+            dest='group_bri', action='store_true')
+        return parser
+
+    def run(self):
+        while True:
+            h = random.randint(*self.opts.hue_range)
+            s = random.randint(*self.opts.sat_range)
+            b = random.randint(*self.opts.bri_range)
+            for L in self.lights:
+                self.bridge.set_light(
+                    L.light_id, {'hue': h, 'sat': s, 'bri': b},
+                    transitiontime=self.opts.cycle_time)
+                if not self.opts.group_hue:
+                    h = random.randint(*self.opts.hue_range)
+                if not self.opts.group_sat:
+                    s = random.randint(*self.opts.sat_range)
+                if not self.opts.group_bri:
+                    b = random.randint(*self.opts.bri_range)
+            time.sleep(self.opts.cycle_time / 10)
+
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        description='Produce a Philips Hue lighting random color fade effect.',
-        epilog='If no lights are specified to be used, all lights found on the '
-        'bridge will be used in the effect. '
-        'The first time this script is run on a device, it may be necessary to '
-        'press the button on the bridge before running the script so that it can '
-        'be authenticated and be permitted to access the bridge and lighting '
-        'system.')
-    parser.add_argument(
-        '-ln', '--light-number',
-        dest='lights', action='append', type=int, metavar='LIGHT-NUM', nargs='+',
-        help='include light number %(metavar)s in the effect')
-    parser.add_argument(
-        '-l', '--light-name',
-        dest='lights', action='append', type=str, metavar='LIGHT-NAME', nargs='+',
-        help='include light named %(metavar)s in the effect')
-    parser.add_argument(
-        '-b', '--bridge',
-        dest='bridge_address',
-        help='IP address or hostname of the Hue bridge to connect to')
-    parser.add_argument(
-        '-t', '--cycle-time',
-        dest='cycle_time', type=int, metavar='DECISECONDS', default=100,
-        help='use a speed of %(metavar)s tenths of a second per color cycle (default: %(default)s)')
-    parser.add_argument(
-        '-gh', '--group-hue',
-        dest='group_hue', action='store_true',
-        help='match the same hue among all lights')
-    parser.add_argument(
-        '-gs', '--group-saturation',
-        dest='group_sat', action='store_true',
-        help='match the same saturation among all lights')
-    parser.add_argument(
-        '-gb', '--group-brightness',
-        dest='group_bri', action='store_true',
-        help='match the same brightness among all lights')
-    parser.add_argument(
-        '-hr', '--hue-range',
-        dest='hue_range', nargs=2, type=int, metavar=('L', 'H'),
-        default=[0, 65535],
-        help='restrict the generated hue range (0 to 65535) from L to H')
-    parser.add_argument(
-        '-sr', '--saturation-range',
-        dest='sat_range', nargs=2, type=int, metavar=('L', 'H'),
-        default=[0, 255],
-        help='restrict the generated saturation range (0 to 255) from L to H')
-    parser.add_argument(
-        '-br', '--brightness-range',
-        dest='bri_range', nargs=2, type=int, metavar=('L', 'H'),
-        default=[1, 254],
-        help='restrict the generated saturation range (1 to 254) from L to H')
-
-    args = parser.parse_args()
-    bridge = get_bridge(args)
-    lights = get_lights(bridge, args)
-    if not lights:
-        print("Error: No lights available", file=sys.stderr)
-        sys.exit(3)
-    turn_on_lights(bridge, lights)
     try:
-        run_show(bridge, args, lights)
-    except KeyboardInterrupt:
-        sys.exit(signal.SIGINT + 128)    # Terminate quietly on ^C
+        p = FadingColorsProgram()
+    except ProgramArgumentError as e:
+        print('Error: %s' % e, file=sys.stderr)
+        sys.exit(2)
+    else:
+        run_with_quit_handler(p)
