7dc8a26837e403951df8f7da90c0140f0e7e8b8e
Travis Evans <travisgevans@gmail.com>
2017-08-17 14:07:16 -0500

    Update coded_digits.py to use base module

--- coded_digits.py
+++ coded_digits.py
@@ -9,6 +9,10 @@ import time
 
 import phue                     # https://github.com/studioimaginaire/phue
 
+from base import (BaseProgram, default_run)
+from fading_colors import FadingColorsProgram
+
+
 # The light parameters used to encode each character/digit
 DIGITS_DEFAULT = 'bright'
 DIGITS = {
@@ -45,110 +49,78 @@ DIGITS = {
 }
 
 
-def get_bridge(args):
-    return phue.Bridge(args.bridge_address)
+class CodedDigitsProgram(FadingColorsProgram):
 
-def get_lights(bridge, args):
-    if args.lights:
-        lights = []
-        for ll in args.lights:
-            for li in ll:
-                try:
-                    lights.append(bridge[li])
-                except KeyError:
-                    print("Warning: No such light: %s" % repr(li), file=sys.stderr)
-    else:
-        lights = bridge.get_light_objects()
-    return lights
+    usage_description = 'Blink out a series of digits encoded using colors.'
 
-def group_digits(digits, num_lights):
-    size_with_pad = math.ceil(len(digits) / num_lights) * num_lights
-    padded_digits = '{:>{}}'.format(digits, size_with_pad)
-    digit_groups = [padded_digits[i:i+num_lights]
-                    for i in range(0, size_with_pad, num_lights)]
-    return digit_groups
+    schemes = DIGITS
 
-def run_show(bridge, args, lights):
-    digit_groups = group_digits(args.digits, len(lights))
-    digit_cmds = DIGITS[args.scheme]
+    default_scheme = DIGITS_DEFAULT
 
-    have_multiple_groups = len(digit_groups) > 1
-    use_padding = args.padded
-    if use_padding is None:
-        use_padding = have_multiple_groups
+    def _get_arg_parser(self):
+        parent = BaseProgram._get_arg_parser(self)
+        parser = argparse.ArgumentParser(parents=[parent], add_help=False)
 
-    for digit_group in digit_groups:
-        if args.switch_time >= 0 and (have_multiple_groups or
-                                      args.force_switch):
-            bridge.set_light([L.light_id for L in lights],
+        self._add_cycle_time_opt(parser, default=10)
+
+        parser.add_argument(
+            '-s', '--switch-time',
+            help='If there are more digits to transmit than lights, display the "blank" color on all lights for %(metavar)s tenths of a second before each digit flash (default: %(default)s . 0 makes it as short as possible; -1 disables it entirely.',
+            dest='switch_time', type=int, metavar='DECISECONDS', default=2)
+        parser.add_argument(
+            '-fs', '--force-switch-time', dest='force_switch',
+            help='always use a switch time after the digit flash, even if there are enough lights to display all digits at once',
+            action='store_true')
+        parser.add_argument(
+            '-p', '--pad',
+            help='always reset all lights to the "blank" color when the sequence finishes, instead of only when there are more digits than lights to transmit',
+            dest='padded', action='store_const', const=True)
+        parser.add_argument(
+            '-np', '--no-pad', dest='padded',
+            help='never reset lights to the "blank" color when the sequence finishes',
+            action='store_const', const=False)
+        parser.add_argument(
+            '-c', '--scheme',
+            help='use the chosen color scheme',
+            dest='scheme', type=str, choices=DIGITS.keys(),
+            default=DIGITS_DEFAULT)
+        parser.add_argument(
+            'digits',
+            help='the sequence of digits to flash',
+            type=str)
+
+        return parser
+
+    def group_digits(self, digits, num_lights):
+        size_with_pad = math.ceil(len(digits) / num_lights) * num_lights
+        padded_digits = '{:>{}}'.format(digits, size_with_pad)
+        digit_groups = [padded_digits[i:i+num_lights]
+                        for i in range(0, size_with_pad, num_lights)]
+        return digit_groups
+
+    def run(self):
+        digit_groups = self.group_digits(self.opts.digits,
+                                         len(self.lights))
+        digit_cmds = self.schemes[self.opts.scheme]
+        have_multiple_groups = len(digit_groups) > 1
+        use_padding = self.opts.padded
+        if use_padding is None:
+            use_padding = have_multiple_groups
+
+        for digit_group in digit_groups:
+            if self.opts.switch_time >= 0 and (have_multiple_groups or
+                                          self.opts.force_switch):
+                self.bridge.set_light([L.light_id for L in self.lights],
+                                 digit_cmds[None], transitiontime=0)
+                time.sleep(self.opts.switch_time / 10)
+            for digit, light in zip(digit_group, self.lights):
+                cmd = digit_cmds.get(digit, digit_cmds[None])
+                self.bridge.set_light(light.light_id, cmd, transitiontime=0)
+            time.sleep(self.opts.cycle_time / 10)
+        if use_padding:
+            self.bridge.set_light([L.light_id for L in self.lights],
                              digit_cmds[None], transitiontime=0)
-            time.sleep(args.switch_time / 10)
-        for digit, light in zip(digit_group, lights):
-            cmd = digit_cmds.get(digit, digit_cmds[None])
-            bridge.set_light(light.light_id, cmd, transitiontime=0)
-        time.sleep(args.cycle_time / 10)
-    if use_padding:
-        bridge.set_light([L.light_id for L in lights],
-                         digit_cmds[None], transitiontime=0)
-        time.sleep(args.cycle_time / 10)
+            time.sleep(self.opts.cycle_time / 10)
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        description='Blink out a series of digits encoded using colors.',
-        epilog='Lights will be sequenced in the order specified. '
-        'If no lights are specified to be used, all lights found on the '
-        'bridge will be used in the effect. '
-        'The first time this script is run on a device, it may be necessary to '
-        'press the button on the bridge before running the script so that it can '
-        'be authenticated and be permitted to access the bridge and lighting '
-        'system.')
-    parser.add_argument(
-        '-ln', '--light-number',
-        dest='lights', action='append', type=int, metavar='LIGHT-NUM', nargs='+',
-        help='include light number %(metavar)s in the sequence')
-    parser.add_argument(
-        '-l', '--light-name',
-        dest='lights', action='append', type=str, metavar='LIGHT-NAME', nargs='+',
-        help='include light named %(metavar)s in the sequence')
-    parser.add_argument(
-        '-b', '--bridge',
-        dest='bridge_address',
-        help='IP address or hostname of the Hue bridge to connect to')
-    parser.add_argument(
-        '-t', '--cycle-time',
-        dest='cycle_time', type=int, metavar='DECISECONDS', default=10,
-        help='display each set of digits for %(metavar)s tenths of a second (default: %(default)s)')
-    parser.add_argument(
-        '-s', '--switch-time',
-        dest='switch_time', type=int, metavar='DECISECONDS', default=2,
-        help='if there are more digits to transmit than lights, display the "blank" color on all lights for %(metavar)s tenths of a second before each digit flash (default: %(default)s . 0 makes it as short as possible; -1 disables it entirely.')
-    parser.add_argument(
-        '-fs', '--force-switch-time', dest='force_switch',
-        action='store_true',
-        help='always use a switch time after the digit flash, even if there are enough lights to display all digits at once')
-    parser.add_argument(
-        '-p', '--pad', dest='padded', action='store_const', const=True,
-        help='always reset all lights to the "blank" color when the sequence finishes, instead of only when there are more digits than lights to transmit')
-    parser.add_argument(
-        '-np', '--no-pad', dest='padded',
-        action='store_const', const=False,
-        help='never reset lights to the "blank" color when the sequence finishes')
-    parser.add_argument(
-        '-c', '--scheme',
-        dest='scheme', type=str, choices=DIGITS.keys(),
-        default=DIGITS_DEFAULT,
-        help='use the chosen color scheme')
-    parser.add_argument(
-        'digits', type=str,
-        help='the sequence of digits to flash')
-
-    args = parser.parse_args()
-    bridge = get_bridge(args)
-    lights = get_lights(bridge, args)
-    if not lights:
-        print("Error: No lights available", file=sys.stderr)
-        sys.exit(3)
-    try:
-        run_show(bridge, args, lights)
-    except KeyboardInterrupt:
-        sys.exit(signal.SIGINT + 128)    # Terminate quietly on ^C
+    default_run(CodedDigitsProgram)
