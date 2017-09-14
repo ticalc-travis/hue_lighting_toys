7b68ab71e158a2785664b789c24bc21e45d32e35
Travis Evans <travisgevans@gmail.com>
2017-09-13 20:18:08 -0500

    Create license and docs in preparation for public release

--- coded_digits.py
+++ coded_digits.py
@@ -1,5 +1,20 @@
 #!/usr/bin/env python3
 
+# Copyright (C) 2017 Travis Evans
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import math
 import time
 
@@ -11,6 +26,8 @@ from chasing_colors import ChasingColorsProgram
 
 DIGITS_DEFAULT = 'bright'
 
+# This dict may be edited to change the light parameters that represent
+# each digit or to add new digit “schemes”
 DIGITS = {
     'dim': {
         '0': {'on': True, 'ct': 250, 'bri': 1},
